82a58191ce92c85c88ada064ca16e910d3ff0e9d
Tomek Buszewski <tomek.buszewski@gmail.com>
2016-01-19 16:26:29 +0100

    Classes

--- _babel/script.js
+++ _babel/script.js
@@ -1,39 +1,37 @@
 const app = angular.module('app', ['ngRoute']);
 
+class ArtistController {
+	constructor($route, $routeParams, $http) {
+		let dataSrc = './data',
+			artist = $routeParams.artist,
+			data = dataSrc+'/'+artist+'.json';
+
+		this.data = data;
+
+		$http.get(data).then(this.renderArtist, this.fail);
+	}
+
+	renderArtist() {
+		console.log(this.data);
+	}
+
+	fail(err) {
+		console.log(err);
+	}
+}
+
+ArtistController.$inject = ['$route', '$routeParams', '$http'];
+
 app.config(($routeProvider) => {
 	$routeProvider
 		.when('/', {
-			controller: 'navCtrl',
 			template: 'Nothing selected'
 		})
 		.when('/music/:artist', {
-			controller: 'artistCtrl',
+			controller: ArtistController,
 			templateUrl: 'tpl/artist.html'
 		})
 		.otherwise({
 			template: '404'
 		});
-});
-
-app.controller('navCtrl', ($scope) => {
-
-});
-
-app.controller('artistCtrl', ($scope, $route, $routeParams, $http) => {
-	let dataSrc = './data',
-		artist = $routeParams.artist;
-
-		let data = dataSrc+'/'+artist+'.json';
-
-	var renderArtist = function(data) {
-		$scope.msg = data.data[0].name+' from '+data.data[0].country;
-		$scope.albums = data.data[0].albums;
-		$scope.loaded = true;
-	};
-
-	var fail = function(err) {
-		console.log(err);
-	};
-
-	$http.get(data).then(renderArtist, fail);
 });
--- _js/all.js
+++ _js/all.js
@@ -1,39 +1,51 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 var app = angular.module('app', ['ngRoute']);
+
+var ArtistController = function () {
+	function ArtistController($route, $routeParams, $http) {
+		_classCallCheck(this, ArtistController);
+
+		var dataSrc = './data',
+		    artist = $routeParams.artist,
+		    data = dataSrc + '/' + artist + '.json';
+
+		this.data = data;
+
+		$http.get(data).then(this.renderArtist, this.fail);
+	}
+
+	_createClass(ArtistController, [{
+		key: 'renderArtist',
+		value: function renderArtist() {
+			console.log(this.data);
+		}
+	}, {
+		key: 'fail',
+		value: function fail(err) {
+			console.log(err);
+		}
+	}]);
+
+	return ArtistController;
+}();
+
+ArtistController.$inject = ['$route', '$routeParams', '$http'];
 
 app.config(function ($routeProvider) {
 	$routeProvider.when('/', {
-		controller: 'navCtrl',
 		template: 'Nothing selected'
 	}).when('/music/:artist', {
-		controller: 'artistCtrl',
+		controller: ArtistController,
 		templateUrl: 'tpl/artist.html'
 	}).otherwise({
 		template: '404'
 	});
-});
-
-app.controller('navCtrl', function ($scope) {});
-
-app.controller('artistCtrl', function ($scope, $route, $routeParams, $http) {
-	var dataSrc = './data',
-	    artist = $routeParams.artist;
-
-	var data = dataSrc + '/' + artist + '.json';
-
-	var renderArtist = function renderArtist(data) {
-		$scope.msg = data.data[0].name + ' from ' + data.data[0].country;
-		$scope.albums = data.data[0].albums;
-		$scope.loaded = true;
-	};
-
-	var fail = function fail(err) {
-		console.log(err);
-	};
-
-	$http.get(data).then(renderArtist, fail);
 });
 
 },{}]},{},[1]);
