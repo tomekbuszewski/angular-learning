325c8348a0a5467592f51de285eb7a98271ff1cb
Tomek Buszewski <tomek.buszewski@gmail.com>
2016-01-22 15:13:33 +0100

    Components

--- _babel/script.js
+++ _babel/script.js
@@ -1,52 +1,18 @@
-const app = angular.module('app', ['ngRoute']);
+const app = angular.module('app', []);
 
-app.config(($routeProvider) => {
-	$routeProvider
-		.when('/', {
-			controller: 'navCtrl',
-			template: 'Nothing selected'
-		})
-		.when('/music/:artist', {
-			controller: 'artistCtrl',
-			templateUrl: 'tpl/artist.html'
-		})
-		.otherwise({
-			template: '404'
-		});
-});
+app.component('shadow', {
+	bindings: {
+		colour: '@'
+	},
+	controller: function() {
+			this.setColour = setColour;
 
-app.controller('navCtrl', ($scope) => {
-	$scope.progress = 'no progress';
-});
-
-app.controller('artistCtrl', ($scope, $route, $routeParams, $http) => {
-	let dataSrc = './data',
-		artist = $routeParams.artist;
-
-	let data = dataSrc+'/'+artist+'.json';
-
-	var renderArtist = function(data) {
-		$scope.msg = data.data[0].name+' from '+data.data[0].country;
-		$scope.albums = data.data[0].albums;
-		$scope.loaded = true;
-		$scope.progress = 'bam';
-	};
-
-	var fail = function(err) {
-		console.log(err);
-	};
-
-	$http.get(data).then(renderArtist, fail);
-});
-
-app.directive('progressBar', () => {
-	return {
-		scope: {},
-		link: (scope, element) => {
-			scope.$watch('progress', () => {
-				element.text(scope.progress);
-				console.log(scope.$parent);
-			});
-		}
-	};
+			function setColour(colour) {
+					this.colour = colour;
+			}
+	},
+	template: ['<div ',
+		'style="background: {{ $ctrl.colour }}; width: 100px; height: 100px;">',
+		'<button ng-click="$ctrl.setColour(\'red\');">Button</button>',
+	'</div>'].join('')
 });
--- _js/all.js
+++ _js/all.js
@@ -1,54 +1,20 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', []);
 
-app.config(function ($routeProvider) {
-	$routeProvider.when('/', {
-		controller: 'navCtrl',
-		template: 'Nothing selected'
-	}).when('/music/:artist', {
-		controller: 'artistCtrl',
-		templateUrl: 'tpl/artist.html'
-	}).otherwise({
-		template: '404'
-	});
-});
+app.component('shadow', {
+	bindings: {
+		colour: '@'
+	},
+	controller: function controller() {
+		this.setColour = setColour;
 
-app.controller('navCtrl', function ($scope) {
-	$scope.progress = 'no progress';
-});
-
-app.controller('artistCtrl', function ($scope, $route, $routeParams, $http) {
-	var dataSrc = './data',
-	    artist = $routeParams.artist;
-
-	var data = dataSrc + '/' + artist + '.json';
-
-	var renderArtist = function renderArtist(data) {
-		$scope.msg = data.data[0].name + ' from ' + data.data[0].country;
-		$scope.albums = data.data[0].albums;
-		$scope.loaded = true;
-		$scope.progress = 'bam';
-	};
-
-	var fail = function fail(err) {
-		console.log(err);
-	};
-
-	$http.get(data).then(renderArtist, fail);
-});
-
-app.directive('progressBar', function () {
-	return {
-		scope: {},
-		link: function link(scope, element) {
-			scope.$watch('progress', function () {
-				element.text(scope.progress);
-				console.log(scope.$parent);
-			});
+		function setColour(colour) {
+			this.colour = colour;
 		}
-	};
+	},
+	template: ['<div ', 'style="background: {{ $ctrl.colour }}; width: 100px; height: 100px;">', '<button ng-click="$ctrl.setColour(\'red\');">Button</button>', '</div>'].join('')
 });
 
 },{}]},{},[1]);
